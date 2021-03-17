85634d43ed7be75db016fb39cfa37a2da6eaad3d
Steve Macintyre <SteveMacintyre868@gmail.com>
2021-03-16 20:31:50 -0700

    Refactored code, fixed an error where player was only supposed to move up to 0, not top border

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,45 +6,46 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    private float _maxHeight, _minHeight, _minWidth, _maxWidth;
+
     
 
     void Start()
     {
         //take the current position = new position(0,0,0)
         transform.position = new Vector3(0,0,0);
+        _maxHeight =  0f;
+        _minHeight = -3.8f;
+        _maxWidth  =  10f;
+        _minWidth  = -_maxWidth;
         
     }
 
     void Update()
     {
+       CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * _speed *  Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
-        //if the position of y is greater than 5.75
-        //y pos will equal 5.75
-        //else if y pos is less than -3.8
-        //y pos is -3.8
         
-        if (transform.position.y > 5.75)
+        if (transform.position.y > _maxHeight)
         {
-            transform.position = new Vector3(transform.position.x,5.75f,0);
-        }else if (transform.position.y < -3.8){
-            transform.position = new Vector3(transform.position.x,-3.8f,0);
+            transform.position = new Vector3(transform.position.x, _maxHeight, 0);
+        }else if (transform.position.y < _minHeight){
+            transform.position = new Vector3(transform.position.x, _minHeight ,0);
         }
 
-        //if x pos is greater than 9
-        //x pos = 9
-        //if x pos is less than -9
-        //x pos is 9
-
-        if (transform.position.x > 9)
+        if (transform.position.x > _maxWidth)
         {
-            transform.position = new Vector3(9f, transform.position.y,0);
-        }else if(transform.position.x < -9)
+            transform.position = new Vector3(_maxWidth, transform.position.y,0);
+        }else if(transform.position.x < _minWidth)
         {
-            transform.position = new Vector3(9f, transform.position.y,0);
+            transform.position = new Vector3(_maxWidth, transform.position.y,0);
         }
 
     }
