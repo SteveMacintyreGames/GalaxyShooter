5cce817bc3098beecffba95d8ea7ab4d38c2abcf
Steve Macintyre <SteveMacintyre868@gmail.com>
2021-04-16 14:18:59 -0700

    Got negative powerup working with new laser system

--- Assets/Scripts/Laser_Enemy.cs
+++ Assets/Scripts/Laser_Enemy.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class Laser_Enemy : Laser
 {
+
+    private GameObject _target;
+    private Vector3 _laserHomingDirection;   
    
     protected override void Start()
     {
         Invoke("DestroyLasers",4f);
         _laserDirection = Vector3.down;
+
+         if(GameManager.Instance._negativePowerupActivated)
+        {
+        _target = GameObject.FindWithTag("Player");
+        _laserHomingDirection = (_target.transform.position - transform.position).normalized;
+        _laserDirection =  new Vector3(_laserHomingDirection.x,-1,0) * _speed * Time.deltaTime;
+        }else
+        {
+            _laserDirection = Vector3.down * _speed * Time.deltaTime;
+        }
     }
+    
 
     protected override void DestroyLasers()
     {
         Debug.Log(transform.parent.gameObject.name);
-        if(transform.parent.gameObject.name == "Enemy_DuoLaser")
+        if(transform.parent.gameObject.name == "Enemy_DuoLaser(Clone)")
         {
             //Debug.Log("EnemyLaser parent destroyed");
             transform.parent.gameObject.SetActive(false);
